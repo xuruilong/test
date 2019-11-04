2d1464c561ff1b3def6a8ffa43cff6931f72d412
lingling <1427583036@qq.com>
2019-11-04 21:50:28 +0800

    把本地工程添加到git

---
+++ src/com/xrl/First.java
@@ -0,0 +1,13 @@
+package com.xrl;
+
+public class First {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+
+    }
+}
